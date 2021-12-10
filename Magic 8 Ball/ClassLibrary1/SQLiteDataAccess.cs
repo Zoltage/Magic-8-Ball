2bb3ddbaeee197e93bc8d62742eb6c4d125832a7
Jacob <jacob.mathewsaz@gmail.com>
2021-12-10 12:14:07 -0700

    somethin

---
+++ Magic 8 Ball/ClassLibrary1/SQLiteDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class SQLiteDataAccess
+    {
+        public static List<PersonModel> LoadPeople()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PersonModel>("select * from Users", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        private static string LoadConnectionString(string id = "Defualt")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
